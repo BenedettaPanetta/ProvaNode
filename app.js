b6bf374de44ba3d31dc6285c7b94d2a807e98bff
BenedettaPanetta <73775977+BenedettaPanetta@users.noreply.github.com>
2021-02-21 16:04:02 +0100

    create app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+var http = require("http");
+
+var express = require("express");
+var apiServer = express();
+
+var port = 3000;
+
+apiServer.listen(port, () => {
+    console.log("server running on port ", port);
+});
+
+apiServer.get("/nome", (req, res) =>{
+    console.log("ho ricevuto una get su nome");
+    res.send("ciao, il nome del server è: NODEPOGLIANI");
+});
+
+apiServer.get("/", function(req, res){
+    res.send("sei in home");
+})
