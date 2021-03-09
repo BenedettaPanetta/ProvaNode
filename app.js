b7ff17eecd406b463a1a76c37a4bd954f2108656
BenedettaPanetta <73775977+BenedettaPanetta@users.noreply.github.com>
2021-03-09 13:19:57 +0100

    Update app.js

--- app.js
+++ app.js
@@ -15,15 +15,13 @@ apiServer.get("/nome", (req, res) =>{
 });
 
 apiServer.get("/somma", (req, res) => {
-    console.log("richiesta", req.quesry);
+    console.log("richiesta: " , req.query);
     if(req.query.a && req.query.b){
-        var ris = req.query.a - (-req.query.b);
-        console.log("ris = ", ris);
-        res.send("{\"risultato"\ : " + ris + "}");
-    }
-    else{
-        res.send("parametri non corretti");
-    }
+        var ris = req.query.a - (- req.query.b); // -> il + concatena, quindi sottraggo l'inverso
+        console.log("ris = " , ris);
+        res.send("{ \"risultato\" : " + ris + "}");
+    }else
+    res.send("parametri non corretti");
 });
 
 apiServer.get("/", function(req, res){
